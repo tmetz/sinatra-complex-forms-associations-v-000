ba38bbee31b8831d67e8dddb675a1a6411cda417
Tammy Metz <tammy@tammymetz.com>
2018-12-22 14:35:08 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(:name => params[:pet][:name])
     if !params[:owner][:name].empty?
-      @pet.owner = Owner.create(:name => params[:pet][:owner][:name])
+      @pet.owner = Owner.create(:name => params[:owner][:name])
     else
       @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
     end
